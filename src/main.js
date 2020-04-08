24971ff96ff3bce20be5514171e2621e6ef55e72
monai@micous.com <monai@micous.com>
2020-04-08 18:55:07 +0800

    markdown 转 html ,JS代码高亮

--- src/main.js
+++ src/main.js
@@ -3,9 +3,20 @@ import App 		from './app.vue';
 import router 	from './router/index';
 import store 	from './store/index';
 import axios 	from './httpClient/axios';
-
+import mavonEditor from 'mavon-editor';
+import hljs from 'highlight.js/lib/highlight';
+import javascript from 'highlight.js/lib/languages/javascript';
+hljs.registerLanguage('javascript', javascript);
+//	httpclient
+Vue.prototype.$hljs = hljs;
 //	css ex: font
-import font 	from './font/iconfont.css';
+import './font/iconfont.css';
+import 'mavon-editor/dist/css/index.css';
+import 'highlight.js/styles/github.css';
+
+
+// use
+Vue.use(mavonEditor)
 
 //	httpclient
 Vue.prototype.$http = axios;
--- src/template/home.vue
+++ src/template/home.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
         <h1>{{ this.vuex.VUEX_homeTitle }}</h1>
+        <p @click='showData'>显示输出数据</p>
+        <div v-html='jsHtml'></div>
+        <mavon-editor @htmlCode='htmlCode' v-model="value"/>
     </div>
 </template>
 <script>
+    import marked from 'marked';
     export default {
         data() {
-            return {}
+            return {
+                value: '',
+                jsHtml: ''
+            }
         },
         computed:{
             vuex(){
@@ -14,13 +21,39 @@
             }
         },
         mounted() {
-            
+            this.jsHtml =  this.$hljs.highlight( 'javascript',`
+
+                    let a = [];
+                    a.__proto__ = Object.prototype;
+                    Object.prototype.toString.call(a)
+                    //  "[object Array]"
+
+                `
+            ).value
+
+
             this.$http.get('/api/web/article/list')
             .then( response =>{
                 console.log( response );
 
                 console.log( this.$store.commit('modify_VUEX_homeTitle', 'mutation 提交') );
             })
+        },
+        methods:{
+            showData(){
+                console.log( this.value );
+            },
+            htmlCode(boolean, md){
+                // marked.setOptions({
+                //     highlight: function(code, lang, callback) {
+                //         require('pygmentize-bundled') ({ lang: lang, format: 'html' }, code, function (err, result) {
+                //         callback(err, result.toString());
+                //         });
+                //     }
+                // })
+                console.log( marked(md) );
+                
+            }
         }
     }
 </script>
